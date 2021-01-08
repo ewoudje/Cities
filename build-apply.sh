9eabc2c1df4b4028e8525fa1d216aec88e88a822
BuildTools <unconfigured@null.spigotmc.org>
2021-01-08 01:30:04 +0100

    Dev Spam

--- build-apply.sh
+++ build-apply.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd ~/ewoudje/cities.git
+
+git checkout dev
+
 mvn clean package install
+
+cd target && sftp://plugin-dev.04d3358d:dev@localhost:2022 "put -O plugins cities-dev.jar; bye"
