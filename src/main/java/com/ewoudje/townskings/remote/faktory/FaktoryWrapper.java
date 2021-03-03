dc9ae8fb1ef37a145cb1db0c836a5a537e5ef44a
ewoud <ewoud.dierickx@hotmail.be>
2021-03-03 23:52:16 +0100

    Per Tick Update

--- src/main/java/com/ewoudje/townskings/remote/faktory/FaktoryWrapper.java
+++ src/main/java/com/ewoudje/townskings/remote/faktory/FaktoryWrapper.java
@@ -4,6 +4,7 @@ import com.ewoudje.townskings.TK;
 import com.ewoudje.townskings.api.TKPlugin;
 import com.github.sikandar.faktory.FaktoryClient;
 import com.github.sikandar.faktory.FaktoryJob;
+import com.github.sikandar.faktory.JobType;
 import io.sentry.Sentry;
 
 import java.io.IOException;
@@ -16,6 +17,15 @@ public class FaktoryWrapper {
         client = new FaktoryClient(
 
         ); //TODO config
+        plugin.getServer().getScheduler()
+                .runTaskTimer(plugin, () -> {
+                    try {
+                        client.push(new FaktoryJob(JobType.of("TownsKings.Repo.Minecraft.Tick"), FaktoryPriority.MC, (o) -> false));
+                    } catch (IOException e) {
+                        Sentry.captureException(e);
+                        e.printStackTrace();
+                    }
+                }, 1, 1);
     }
 
     public void push(FaktoryJob job) {
